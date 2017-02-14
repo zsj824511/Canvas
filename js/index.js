ac95a7366fc4c315ee3b8ac98f3235c772d1552f
zhoushuaijun <zhouhao@qq.com>
2017-02-14 19:33:57 +0800

    增加了一条直线

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 * @Author: LarryZhou
 * @Date:   2017-02-14 19:16:29
 * @Last Modified by:   LarryZhou
-* @Last Modified time: 2017-02-14 19:24:09
+* @Last Modified time: 2017-02-14 19:31:27
 */
 
 'use strict';
@@ -14,3 +14,7 @@ ctx.beginPath();
 ctx.arc(100, 100, 100,0,2*Math.PI);
 ctx.fillStyle="blue";
 ctx.fill();
+ctx. beginPath();
+ctx.moveTo(100, 100);
+ctx.lineTo(400, 300);
+ctx.stroke();
